b8b2c123524175bcbbaa0a5b9399cd4b08bf6b56
njps3 <john.snowdon@newcastle.ac.uk>
2016-10-25 17:13:30 +0100

    add asset extractor script - just prints for now, doesnt actually extract the blocks

--- extractAssets.py
+++ extractAssets.py
@@ -26,6 +26,7 @@ John Snowdon <john@target-earth.net>
 """
 
 import os
+import time
 import codecs
 import sys
 import traceback
@@ -41,11 +42,38 @@ import json
 VERBOSE = False
 OVERWRITE = False
 ROM_NAME = "Cyber Knight (J).pce"
-ASSET_TABLE = "CyberKnight Asset Banks.csv"
 OUT_NAME = "CyberKnight Assets.json"
 
-ASSETS = []
-ASSET_BANKS = []
+from CyberKnightAssetBanks import ASSETS, ASSET_LOAD_TABLE, ASSET_OFFSET_TABLE
+from CyberKnightAssetBanks import ASSET_LOAD_TABLE_SIZE, ASSET_OFFSET_TABLE_SIZE
+
+ASSET_BANKS = ASSETS["asset_banks"].keys()
+
+######################################################
+
+def find_next_address(upper_address = None, current_asset = None, assets = []):
+	""" Return the upper address for a given asset """
+
+	# Next address defaults to the upper region for this asset bank
+	next_address = upper_address
+	lowest_asset_address = upper_address
+	for asset in assets:
+			
+			## Skip anything with a lower starting address than our current asset
+			if asset["asset_rom_pointer_address"] <= current_asset["asset_rom_pointer_address"]:
+				pass
+			else:
+				# If this asset has a lower address than the end address of this asset bank
+				# then it could be the next in sequence, we'll know once we've looped through
+				# all remaining assets
+				if (asset["asset_rom_pointer_address"] <= lowest_asset_address) and (asset["asset_index"] != current_asset["asset_index"]):
+					lowest_asset_address = asset["asset_rom_pointer_address"]
+					next_address = lowest_asset_address
+				else:
+					pass
+
+	print("-----> %s: %s - %s [%s bytes]" % (hex(current_asset["asset_index"]), hex(current_asset["asset_rom_pointer_address"]), hex(next_address), (next_address - current_asset["asset_rom_pointer_address"])))
+	return next_address
 
 ######################################################
 ########## < Run-time code start here > ##############
@@ -57,26 +85,26 @@ except getopt.GetoptError as err:
 	print(err)
 	sys.exit(2)
 
-print ""
-print "extractAssets.py - Asset block extractor for Cyber Knight"
-print "----------------"
-print ""
+print("")
+print("extractAssets.py - Asset block extractor for Cyber Knight")
+print("----------------")
+print("")
 
 for o, a in opts:
 	if o == "-h":
-		print "A simple tool for extracting asset blockse from the game 'Cyber Knight' for the PC-Engine."
-		print "The tool scans the asset blocks as described within 'CyberKnight Asset Banks.csv]"
-		print ""
-		print "Options:"
-		print "-h	Show help text"
-		print "-v	Enable verbose output"
-		print "-i	Input file name (e.g. 'Cyber Knight (J).pce')"
-		print "-o	Output file name (e.g. 'Cyber Knight.json')"
-		print "-f	Force overwite of output file even if it already exists"
-		print ""
-		print "Example:"
-		print "extractAssets.py -i 'Cyber Knight (J).pce' -o 'CyberKnight Assets.json'"
-		print ""
+		print("A simple tool for extracting asset blockse from the game 'Cyber Knight' for the PC-Engine.")
+		print("The tool scans the asset blocks as described within 'CyberKnightAssets.py")
+		print("")
+		print("Options:")
+		print("-h	Show help text")
+		print("-v	Enable verbose output")
+		print("-i	Input file name (e.g. 'Cyber Knight (J).pce')")
+		print("-o	Output file name (e.g. 'Cyber Knight.json')")
+		print("-f	Force overwite of output file even if it already exists")
+		print("")
+		print("Example:")
+		print("extractAssets.py -i 'Cyber Knight (J).pce' -o 'CyberKnight Assets.json'")
+		print("")
 		sys.exit(0)
 		
 	if o == "-v":
@@ -85,18 +113,11 @@ for o, a in opts:
 	if o == "-i":
 		ROM_NAME = a
 
-	if o == "-t":
-		ASSET_TABLE = a
-
 	if o == "-o":
 		OUT_NAME = a
 		
 	if o == "-f":
 		OVERWRITE = True
-		
-######################################################
-########## < Run-time code start here > ##############
-######################################################
 
 print("Configuration")
 print("=============")
@@ -107,40 +128,50 @@ if os.path.isfile(ROM_NAME):
 else:
 	print("Input ROM File: %s <- ERROR, input file not found!" % ROM_NAME)
 	sys.exit(2)
-if os.path.isfile(ASSET_TABLE):
-	print("Asset Table File: %s <- OK" % ASSET_TABLE)
-else:
-	print("Asset Table File: %s <- ERROR, Asset Table file not found!" % ASSET_TABLE)
-	sys.exit(2)	
 	
 print("Output File: %s" % OUT_NAME)
 print("")
 
 try:
 	file_rom = open(ROM_NAME, "ro")
-	file_table = open(ASSET_TABLE, "ro")
-	
-	# First get a unique list of asset bank id's
-	for l in file_table:
-		asset_bank = l.split(',')[0]
-		ASSET_BANKS.append(asset_bank)
-		
-	asset_ids = list(set(ASSET_BANKS))
-	asset_ids.sort()
-	print(asset_ids)
-	
-	# Find all the asset pointers for each asset bank
-	for asset_id in asset_ids:
-		for l in file_table:
-			asset_bank_id = l.split(',')[0]
-			if asset_bank_id == asset_id:
-				pass
-				
-		#asset_index = l.split(',')[1]
-		#asset_pointer = l.split(',')[2]
-		#asset_block_start = l.split(',')[3]
-		#asset_block_end = l.split(',')[4]
-		#asset_rom_location = l.rstrip().split(',')[5]
-		#print(asset_rom_location)
 except Exception as e:
 	print(e)
+
+print("There are %s asset banks defined" % len(ASSET_BANKS))
+for ab in ASSET_BANKS:
+	print("Bank: %s" % hex(ab))
+	print("---> Contains %s asset pointers" % len(ASSETS["asset_banks"][ab]["assets"].keys()))
+	print("---> Region %s - %s" % (hex(ASSETS["asset_banks"][ab]["asset_bank_rom_start_address"]), hex(ASSETS["asset_banks"][ab]["asset_bank_rom_end_address"])))
+	# In order to know where to start we need to know the lowest asset chunk, 
+	# and its not always the first asset number.
+	lowest_asset_address = ASSETS["asset_banks"][ab]["asset_bank_rom_end_address"]
+	lowest_asset_number = 0x0
+	
+	asset_sequence = []
+	
+	for asset_index in ASSETS["asset_banks"][ab]["assets"].keys():		
+		# Is this the lowest asset start address for this bank?
+		if ASSETS["asset_banks"][ab]["assets"][asset_index]["asset_rom_pointer_address"] < lowest_asset_address:
+			lowest_asset_address = ASSETS["asset_banks"][ab]["assets"][asset_index]["asset_rom_pointer_address"]
+			lowest_asset_number = asset_index
+			
+	lowest_asset = ASSETS["asset_banks"][ab]["assets"][lowest_asset_number]
+	asset_sequence.append(lowest_asset)
+	print("---> Starting asset chunk (%s) located at: %s" % (hex(lowest_asset_number), hex(lowest_asset_address)))
+	print("---> Finding next asset sequence")
+			
+	# For each asset, find the upper limit of the region it can be assumed to use
+	# by finding the start address of the next highest asset in the bank.
+	processed_assets = []
+	for asset in ASSETS["asset_banks"][ab]["assets"].values():	
+		next_address = find_next_address(upper_address = ASSETS["asset_banks"][ab]["asset_bank_rom_end_address"], current_asset = asset, assets = ASSETS["asset_banks"][ab]["assets"].values())
+		asset_sequence.append(next_address)			
+		asset["asset_rom_pointer_address_limit"] = next_address
+		processed_assets.append(asset)
+
+	# Processed assets now contains a list of assets for this bank that have the 
+	# upper limit address embedded, so we know how big each of them are!
+	#print processed_assets
+	print("")
+	time.sleep(2)
+	
